51b573d22631016d91f06f81a85d41957587ae05
AnandDev <anandrathi.dev@gmail.com>
2017-11-22 23:04:19 +0530

    Update AlgorithmsSearch_GridlandMetro.py

--- AlgorithmsSearch_GridlandMetro.py
+++ AlgorithmsSearch_GridlandMetro.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+@author: anandrathi
+"""
+
 from collections import Counter
 from sys import stdin
 
@@ -6,8 +11,8 @@ class railTrack:
         self.c1=c1
         self.c2=c2
 
-def countCells(r,c1,c2):
-    
+def countCells(tracks):
+    return sum([ (k.c2 - k.c1)+1  for k in tracks.values()])
 
 def main():
     lines = list()
@@ -15,10 +20,20 @@ def main():
     for line in stdin:
         lines.append(line.strip())
     r,c,k = lines[0].split(' ')
-    print(r,c,k)
+    #print(r,c,k)
     for i in range(1, k):
         trR,c1,c2 = lines[i].split()
-        tracksHash[trR] 
+        node = tracksHash.get(trR)
+        if node == None:
+            tracksHash[trR] = railTrack(c1,c2)
+        else:
+            tracksHash[trR].c1 = min( (c1,tracksHash[trR].c1 )  )
+            tracksHash[trR].c2 = max( (c2,tracksHash[trR].c2 )  )
+    occupied = countCells(tracksHash)
+    total = r*c 
+    freec = total - occupied
+    print(freec)
 
 if __name__ == '__main__':
 	main()
+    
